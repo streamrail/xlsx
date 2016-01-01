9fb52bd2b0c21aa87d96dec11c3e851ef1a80c58
Or Hiltch <or@streamrail.com>
2016-01-01 18:30:15 +0200

    add csv conv

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # xlsx
 
-convenience package over `github.com/tealeg/xlsx` to transform generic data into an excel sheet. may be used with a BQ result (see reporting_backend project).
+convenience package over `github.com/tealeg/xlsx` to transform generic data into an excel sheet or csv. may be used with a BQ result (see reporting_backend project).
---
+++ csv.go
@@ -0,0 +1,41 @@
+package xlsx
+
+import (
+	"fmt"
+	"strings"
+
+	"bufio"
+	"bytes"
+	"github.com/tealeg/xlsx"
+)
+
+func ToCSV(bs []byte, sheetIndex int, delimiter string) ([]byte, error) {
+	xlFile, err := xlsx.OpenBinary(bs)
+
+	if err != nil {
+		return nil, err
+	}
+	sheetLen := len(xlFile.Sheets)
+	switch {
+	case sheetLen == 0:
+		return nil, fmt.Errorf("This XLSX file contains no sheets.")
+	case sheetIndex >= sheetLen:
+		return nil, fmt.Errorf("No sheet %d available, please select a sheet between 0 and %d\n", sheetIndex, sheetLen-1)
+	}
+	sheet := xlFile.Sheets[sheetIndex]
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+
+	for _, row := range sheet.Rows {
+		var vals []string
+		if row != nil {
+			for _, cell := range row.Cells {
+				vals = append(vals, fmt.Sprintf("%q", cell.String()))
+			}
+			writer.WriteString(strings.Join(vals, delimiter) + "\n")
+		}
+	}
+	writer.Flush()
+	return b.Bytes(), nil
+}
